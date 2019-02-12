59aa7599fcc7b692eb82a29123eee1f376c35738
jamalbelilet <fd_belilet@esi.dz>
2019-02-12 19:41:16 +0100

    5. Add a UI for composing messages

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,58 @@ class FriendlychatApp extends StatelessWidget {
       );
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => ChatScreeState();
+}
+
+class ChatScreeState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('Friendlychat'),
+      appBar: AppBar(
+        title: Text('Friendlychat'),
+      ),
+      body: Column(children: <Widget>[
+        Row(
+          children: <Widget>[
+            _buildTextComposer(),
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: null,
+            )
+          ],
+        )
+      ]));
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
 }
